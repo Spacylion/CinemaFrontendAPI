760d87afda6108a5144dbb40c371b60a0c78beac
German Kosach <fuerstein@yandex.ru>
2023-08-03 19:14:13 +0300

    1 day

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import * as path from "path"
+import * as webpack from "webpack"
+import HTMLWebpackPlugin from "html-webpack-plugin"
+
+const ProgressPlugin = require("webpack/lib/ProgressPlugin")
+
+const config: webpack.Configuration = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src", "index.ts"),
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new ProgressPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+}
+
+export default config
