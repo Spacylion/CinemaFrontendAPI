79b970e8d0f7b4d5f2ee0e639409fa4dbdc4da79
German Kosach <fuerstein@yandex.ru>
2023-08-04 23:50:38 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
 import { render } from "react-dom"
-import { Counter } from "./components/Counter"
 
-render(
-  <div>
-    <Counter />
-  </div>,
-  document.getElementById("root")
-)
+render(<div>sfs </div>, document.getElementById("root"))
